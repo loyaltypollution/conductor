537801d4b3d6ae84bee346932433304348511dbe
loyaltypollution <65063925+loyaltypollution@users.noreply.github.com>
2025-08-23 20:09:18 +0000

    Provide index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Export plugins
+export { BasicHostPlugin } from './conductor/host/BasicHostPlugin';
+export { IHostPlugin } from './conductor/host/types';
+
+// Export conduit
+export { Conduit } from './conduit';
+export { IConduit } from './conduit/types';
+export { IChannel, IPlugin, PluginClass, Subscriber } from './conduit/types';
